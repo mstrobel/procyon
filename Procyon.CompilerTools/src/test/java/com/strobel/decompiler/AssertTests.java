9ac3db22ba1168e8e6f83526fa06171793fdbd41
Mike Strobel <mike.strobel@gmail.com>
2013-09-04 18:05:09 -0400

    Improved assert tests.

--- Procyon.CompilerTools/src/test/java/com/strobel/decompiler/AssertTests.java
+++ Procyon.CompilerTools/src/test/java/com/strobel/decompiler/AssertTests.java
@@ -11,6 +11,12 @@ public class AssertTests extends DecompilerTest {
 
     private static class B {
         public void test(final String s) {
+            assert s.equals("foo") : "Expected 'foo'.";
+        }
+    }
+
+    private static class C {
+        public void test(final String s) {
             assert s.equals("foo") : "Expected 'foo', got: " + s;
         }
     }
@@ -29,11 +35,23 @@ public class AssertTests extends DecompilerTest {
     }
 
     @Test
-    public void testAssertWithExpressionMessage() throws Throwable {
+    public void testAssertWithLiteralMessage() throws Throwable {
         verifyOutput(
             B.class,
             defaultSettings(),
             "private static class B {\n" +
+            "    public void test(final String s) {\n" +
+            "        assert s.equals(\"foo\") : \"Expected 'foo'.\";\n" +
+            "    }\n" +
+            "}\n"
+        );
+    }
+    @Test
+    public void testAssertWithExpressionMessage() throws Throwable {
+        verifyOutput(
+            C.class,
+            defaultSettings(),
+            "private static class C {\n" +
             "    public void test(final String s) {\n" +
             "        assert s.equals(\"foo\") : \"Expected 'foo', got: \" + s;\n" +
             "    }\n" +
