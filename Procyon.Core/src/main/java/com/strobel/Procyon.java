561abe3c0d4e15cd27fb83f96a586d365b421a68
mstrobel <mstrobel@mobile-strobel.local>
2015-09-06 22:45:51 -0400

    Updated build version to v0.5.31.

--- Procyon.Core/src/main/java/com/strobel/Procyon.java
+++ Procyon.Core/src/main/java/com/strobel/Procyon.java
@@ -14,7 +14,7 @@
 package com.strobel;
 
 public final class Procyon {
-    private static final String VERSION = "1.0-SNAPSHOT";
+    private static final String VERSION = "0.5.31";
 
     public static String version() {
         return VERSION;
