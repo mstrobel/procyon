dfc5ed9b6391443838a042634f1032a4aafdf5df
Mike Strobel <mike.strobel@gmail.com>
2017-09-09 11:58:17 -0400

    flow: Created branch 'feature/legacy-class-constants'.

---
+++ Procyon.CompilerTools/src/main/java/com/strobel/decompiler/languages/java/ast/transforms/RewriteLegacyClassConstantsTransform.java
@@ -0,0 +1,14 @@
+package com.strobel.decompiler.languages.java.ast.transforms;
+
+import com.strobel.decompiler.DecompilerContext;
+import com.strobel.decompiler.languages.java.ast.AstNode;
+
+public class RewriteLegacyClassConstantsTransform implements IAstTransform {
+    public RewriteLegacyClassConstantsTransform(final DecompilerContext context) {
+    }
+
+    @Override
+    public void run(final AstNode compilationUnit) {
+        
+    }
+}
