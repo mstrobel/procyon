b75f0da90eab1d6248777a4ed0f2c3d97693f86d
nbauma109 <nbauma109@github.com>
2022-06-05 11:54:04 +0200

    Created InMemoryLineNumberFormatter to avoid temp file creation

--- Procyon.Decompiler/src/main/java/com/strobel/decompiler/DecompilerDriver.java
+++ Procyon.Decompiler/src/main/java/com/strobel/decompiler/DecompilerDriver.java
@@ -1,3 +1,19 @@
+/*
+ * DecompilerDriver.java
+ *
+ * Copyright (c) 2013-2022 Mike Strobel and other contributors
+ *
+ * This source code is based on Mono.Cecil from Jb Evain, Copyright (c) Jb Evain;
+ * and ILSpy/ICSharpCode from SharpDevelop, Copyright (c) AlphaSierraPapa.
+ *
+ * This source code is subject to terms and conditions of the Apache License, Version 2.0.
+ * A copy of the license can be found in the License.html file at the root of this distribution.
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the
+ * Apache License, Version 2.0.
+ *
+ * You must not remove this notice, or any other, from this software.
+ */
+
 package com.strobel.decompiler;
 
 import com.beust.jcommander.JCommander;
@@ -15,7 +31,6 @@ import com.strobel.assembler.metadata.TypeDefinition;
 import com.strobel.assembler.metadata.TypeReference;
 import com.strobel.core.ExceptionUtilities;
 import com.strobel.core.StringUtilities;
-import com.strobel.decompiler.LineNumberFormatter.LineNumberOption;
 import com.strobel.decompiler.languages.BytecodeLanguage;
 import com.strobel.decompiler.languages.BytecodeOutputOptions;
 import com.strobel.decompiler.languages.Languages;
--- Procyon.Decompiler/src/main/java/com/strobel/decompiler/InMemoryLineNumberFormatter.java
+++ Procyon.Decompiler/src/main/java/com/strobel/decompiler/InMemoryLineNumberFormatter.java
@@ -1,5 +1,5 @@
 /*
- * LineNumberFormatter.java
+ * InMemoryLineNumberFormatter.java
  *
  * Copyright (c) 2013-2022 Mike Strobel and other contributors
  *
@@ -19,41 +19,40 @@ package com.strobel.decompiler;
 import com.strobel.decompiler.languages.LineNumberPosition;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.StringReader;
 import java.util.EnumSet;
 import java.util.List;
 
 /**
- * A <code>LineNumberFormatter</code> is used to rewrite an existing .java file, introducing
+ * An <code>InMemoryLineNumberFormatter</code> is used to rewrite an existing .java source, introducing
  * line number information.  It can handle either, or both, of the following jobs:
  * 
  * <ul>
  * <li>Introduce line numbers as leading comments.
- *   <li>Stretch the file so that the line number comments match the physical lines.
+ *   <li>Stretch the source so that the line number comments match the physical lines.
  * </ul>
  */
-public class LineNumberFormatter extends AbstractLineNumberFormatter {
+public class InMemoryLineNumberFormatter extends AbstractLineNumberFormatter {
 
-    private final File _file;
+    private final String _source;
 
     /**
      * Constructs an instance.
      * 
-     * @param file                the file whose line numbers should be fixed
-     * @param lineNumberPositions a recipe for how to fix the line numbers in 'file'.
-     * @param options controls how 'this' represents line numbers in the resulting file
+     * @param source                the source whose line numbers should be fixed
+     * @param lineNumberPositions a recipe for how to fix the line numbers in source.
+     * @param options controls how 'this' represents line numbers in the resulting source
      */
-    public LineNumberFormatter(File file,
+    public InMemoryLineNumberFormatter(String source,
             List<LineNumberPosition> lineNumberPositions,
             EnumSet<LineNumberOption> options) {
         super(lineNumberPositions, options);
-        _file = file;
+        _source = source;
     }
 
     @Override
     protected BufferedReader createReader() throws IOException {
-        return Files.newBufferedReader(_file.toPath());
+        return new BufferedReader(new StringReader(_source));
     }
 }
